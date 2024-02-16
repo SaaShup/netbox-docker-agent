20e20fb64364f1f9ef5ed96957b6f4fd667f5f61
LaV <lav@saashup.com>
2024-02-16 09:04:37 +0100

    add dev settings

---
+++ settings_dev.js
@@ -0,0 +1,108 @@
+data = require("./package.json")
+
+module.exports = {
+    credentialSecret: "saashup",
+    flowFile: "flows.json",
+    flowFilePretty: true,
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2a$08$s.NFdSn4Gm4d7gHErya//e6O8RO1/3f7TZ7zflXJ9jfFV0cI6jGwK",
+            permissions: "*"
+        }]
+    },
+    /*https: {
+      key: require("fs").readFileSync('/data/privkey.pem'),
+      cert: require("fs").readFileSync('/data/cert.pem')
+    },
+    requireHttps: true,*/
+    httpNodeAuth: {
+        user:"admin",pass:"$2a$08$s.NFdSn4Gm4d7gHErya//e6O8RO1/3f7TZ7zflXJ9jfFV0cI6jGwK"
+    },
+    uiPort: process.env.PORT || 1880,
+    httpStatic: [
+        { path: './public', root: '/' },
+        { path: './public/doc.html', root: '/doc' },
+        { path: './public/openapi.yml', root: '/openapi' }
+    ],
+    httpAdminRoot: '/nodered',
+    diagnostics: {
+        enabled: true,
+        ui: true,
+    },
+    runtimeState: {
+        enabled: false,
+        ui: false,
+    },
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false,
+            handler: function(settings) {
+                return function(msg) {
+                    const level = {
+                        20: 'error',
+                        30: 'warn',
+                        40: 'info'
+                    };
+                    const lvl = "level" in msg ? msg.level : "40";
+
+                    delete msg.type;
+                    delete msg.z;
+                    delete msg.path;
+                    delete msg.name;
+                    delete msg.id;
+
+                    let line = `${data.name} level=${level[lvl]} version=${data.version}`;
+
+                    if (typeof msg.msg === 'object') {
+                        for (const key of Object.keys(msg.msg)) {
+                            line += ` ${key}=${JSON.stringify(msg.msg[key])}`;
+                        }
+                    } else {
+                        line += ` msg=` + JSON.stringify(msg.msg);
+                    }
+
+                    if (lvl <= 20) {
+                        return console.error(line);
+                    }
+
+                    return console.log(line);
+                }
+            }
+        }
+    },
+    exportGlobalContextKeys: false,
+    externalModules: {
+    },
+    editorTheme: {
+        tours: false,
+        palette: {
+        },
+        projects: {
+            enabled: false,
+            workflow: {
+                mode: "manual"
+            }
+        },
+        codeEditor: {
+            lib: "monaco",
+            options: {
+            }
+        },
+        markdownEditor: {
+            mermaid: {
+                enabled: true
+            }
+        },
+    },
+    functionExternalModules: true,
+    functionTimeout: 0,
+    functionGlobalContext: {
+    },
+    debugMaxLength: 1000,
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000
+}
