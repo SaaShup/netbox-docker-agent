d1425c10a144a2a96a71b800b36900f1c802353e
David Delassus <david.jose.delassus@gmail.com>
2024-03-27 18:42:08 +0100

    :sparkles: create host path for bind mounts

--- pkg/controller/messages/container-recreate.go
+++ pkg/controller/messages/container-recreate.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
@@ -91,6 +92,25 @@ func (m *ContainerRecreateRequest) Handle(client client.APIClient) error {
 	}
 
 	for _, bindSpec := range m.Spec.Binds {
+		err := os.MkdirAll(bindSpec.HostPath, 0755)
+		if err != nil {
+			slog.ErrorContext(
+				m.Context,
+				err.Error(),
+				"container.name", m.Spec.Name,
+				"container.image", m.Spec.Image,
+				"container.mount.type", "bind",
+				"container.mount.host_path", bindSpec.HostPath,
+				"container.mount.container_path", bindSpec.ContainerPath,
+			)
+
+			m.ReplyTo <- ContainerRecreateResponse{
+				ContainerID: "",
+				Err:         err,
+			}
+			return nil
+		}
+
 		mounts = append(mounts, mount.Mount{
 			Type:   mount.TypeBind,
 			Source: bindSpec.HostPath,
